b8ba03479542995b620855533b449d143045f16c
Tom Sell <thomas.j.sell@gmail.com>
2015-02-06 18:38:00 -0500

    removed unnecessary code

--- Assets/Scripts/ToggleFlashlight.cs
+++ Assets/Scripts/ToggleFlashlight.cs
@@ -4,19 +4,14 @@ using System.Collections;
 public class ToggleFlashlight : MonoBehaviour
 {
 
-  // Use this for initialization
-  void Start ()
-  {
-//    gameObject.SetActive (true);
-  }
-	
   // Update is called once per frame
   void Update ()
   {
-    if (Input.GetKeyDown ("q"))
-    if (light.intensity == 0)
-      light.intensity = 2;
-    else
-      light.intensity = 0;
+    if (Input.GetKeyDown ("q")) {
+      if (light.intensity == 0)
+        light.intensity = 2;
+      else
+        light.intensity = 0;
+    }
   }
 }
