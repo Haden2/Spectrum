6e2a8c01b2d9da16fe89eacd1c16cbf870d30ec3
Tom Sell <thomas.j.sell@gmail.com>
2015-02-06 18:42:51 -0500

    added public variable so flashlight intensity can be tweaked within the unity editor

--- Assets/Scripts/ToggleFlashlight.cs
+++ Assets/Scripts/ToggleFlashlight.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class ToggleFlashlight : MonoBehaviour
 {
+  public float lightIntensity;
 
   // Update is called once per frame
   void Update ()
   {
     if (Input.GetKeyDown ("q")) {
       if (light.intensity == 0)
-        light.intensity = 2;
+        light.intensity = lightIntensity;
       else
         light.intensity = 0;
     }
