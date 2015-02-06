61c51127780f9c4af3616efa0959bd88af1a43df
Haden2 <hadext10@gmail.com>
2015-02-06 14:33:44 -0500

    All folders from Unity
    Copying over all folders from Unity to Git/Master branch

---
+++ Assets/Scripts/CollectItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CollectItem : MonoBehaviour {
+
+	public Text pressE;
+	public Color pickupText = new Color(1f, 0f, 0f, 1f);
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			pressE.color = pickupText;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//public float speed;
+	public Text pressE;
+	public Color pickupText = new Color(1f, 0f, 0f, 1f);
+	
+	// Update is called once per frame
+/*	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+
+	}
+*/
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			pressE.color = pickupText;
+			//other.gameObject.SetActive(false);
+		}
+	}
+}
