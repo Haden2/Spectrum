8e93e4db35dcdb5f5317ec5b4411a9bceff80eb7
Haden2 <hadext10@gmail.com>
2015-02-07 16:15:02 -0500

    Minor Changes
    Changed location of the player, added a tag for the floor to help with
    navigation later, and messed around with ideas and code to remove the
    press E text. I know how to do it, but I will be downtown tonight. If I
    get a minute, I know how to do it.

--- Assets/Scripts/CollectItem.cs
+++ Assets/Scripts/CollectItem.cs
@@ -7,6 +7,7 @@ public class CollectItem : MonoBehaviour {
 	public Text pressE;
 	public Color pickupText = new Color(1f, 0f, 0f, 1f);
 	public float flashSpeed = 5f;
+	public Color blank = new Color (0f, 0f, 0f, 0f);
 
 	void OnTriggerEnter(Collider other) 
 	{
@@ -16,7 +17,7 @@ public class CollectItem : MonoBehaviour {
 		} 
 		else //if (other.gameObject.tag (Doesn't equal) "PickUp")
 		{
-			pressE.color = Color.Lerp (pressE.color, Color.clear, flashSpeed * Time.deltaTime);
+			pressE.color = blank;
 		}
 	}
 }
