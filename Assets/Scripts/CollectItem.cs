cbdcc411728e5d92624cab2f9845622384ce6704
Haden2 <hadext10@gmail.com>
2015-02-07 04:05:32 -0500

    Removing Press E Text
    Worked on the if/else code to prevent the Press E to pick up text when
    not touching the item. Couldn't get it to work in the short time I
    worked on it. Can work on it again soon

--- Assets/Scripts/CollectItem.cs
+++ Assets/Scripts/CollectItem.cs
@@ -6,12 +6,17 @@ public class CollectItem : MonoBehaviour {
 
 	public Text pressE;
 	public Color pickupText = new Color(1f, 0f, 0f, 1f);
+	public float flashSpeed = 5f;
 
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.tag == "PickUp") 
 		{
 			pressE.color = pickupText;
+		} 
+		else //if (other.gameObject.tag (Doesn't equal) "PickUp")
+		{
+			pressE.color = Color.Lerp (pressE.color, Color.clear, flashSpeed * Time.deltaTime);
 		}
 	}
 }
