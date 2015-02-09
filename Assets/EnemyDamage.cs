7c0db4474df28a374d2c4b72f25c8c495601c512
Haden2 <hadext10@gmail.com>
2015-02-09 16:28:20 -0500

    Attempted to cause collider to do something
    Moved EnemyMovement script. Added EnemyDamage script. What I am trying
    to do is when the collider reaches the player, turn the light intensity
    to 0. I am having troubles referencing the flashlight from the other
    script though. Or maybe I dont do it from the script. Maybe I can do it
    straight from the game object?

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour 
+{
+	GameObject player;
+	ToggleFlashlight lightIntensity;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		lightIntensity = GetComponent <ToggleFlashlight>();
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if(other.gameObject == player)
+		{
+			light.intensity = 0;
+		}
+
+	}
+	
+
+}
