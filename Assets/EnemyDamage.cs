3aee1e99d2502f2e24fe458ea879f3eadf3dbc61
Haden2 <hadext10@gmail.com>
2015-02-12 15:15:45 -0500

    Substantial updates to enemy
    I re-baked the nav mesh so now the enemy avoids all obstacles. When I
    was messing with the environment before, I was sad to see it run into a
    wall and stop chasing the player. So now it can smartly figure out the
    best path. It cannot jump down however because that is only available in
    pro. I created a boolean to make it so it changes navigation when near
    the player. And I also increased the speed of the enemy to 10 when it
    changes its navigation path. Probably later tonight I will set up the
    reverse for when the player touches the enemy to get the glasses back. I
    am also very forgetful about making a new branch and I keep just making
    edits straight to master. Sorry about that. I will work on it.

--- Assets/EnemyDamage.cs
+++ Assets/EnemyDamage.cs
@@ -6,10 +6,19 @@ public class EnemyDamage : MonoBehaviour
 {
 	public GameObject BlueFlashlight;
 	GameObject player; 
+	Transform escapeDestination;
+	NavMeshAgent nav;
+	NavMeshAgent turnUp;
+
+	bool escape;
+
 
 	void Awake()
 	{
 		player = GameObject.FindGameObjectWithTag ("Player");
+		escapeDestination = GameObject.FindGameObjectWithTag ("Respawn").transform;
+		nav = GetComponent<NavMeshAgent>();
+		turnUp = GetComponent <NavMeshAgent>();
 	}
 
 	void OnTriggerEnter (Collider other) 
@@ -17,9 +26,19 @@ public class EnemyDamage : MonoBehaviour
 		if(other.gameObject == player)
 		{
 			BlueFlashlight.gameObject.SetActive(false);
+			nav.SetDestination (escapeDestination.position);
+			escape = true;
 		}
 
 	}
 
+	void Update()
+	{
+		if(escape)
+		{
+			nav.SetDestination(escapeDestination.position);
+			turnUp.speed = 10;
+		}
+	}
 
 }
