b1c3b11a06bd2d3ef1ccfaf0e90ff99d372e210e
Haden2 <hadext10@gmail.com>
2015-02-13 16:31:26 -0500

    Added lens flare to light.
    Got it to turn off when the enemy reaches the player, and I also got it
    to turn off when pressing q. Couldn't get it to turn back on though
    before going to work. Feel free to edit with other flares. The welding
    flare looked pretty cool too.

--- Assets/Scripts/ToggleFlashlight.cs
+++ Assets/Scripts/ToggleFlashlight.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class ToggleFlashlight : MonoBehaviour
 {
   public float lightIntensity;
+	public  LensFlare lf;
+	public float lensFlare;
 
   // Update is called once per frame
   void Update ()
@@ -11,9 +13,13 @@ public class ToggleFlashlight : MonoBehaviour
     if (Input.GetKeyDown ("q")) 
 	{
       if (light.intensity == 0) //If the light was off, turn it on.
-        light.intensity = lightIntensity;
+       { 
+			light.intensity = lightIntensity;
+			lf.brightness = lensFlare;
+		}
       else
-        light.intensity = 0; //If the light was on, turn it off.
+      	 	light.intensity = 0; //If the light was on, turn it off.
+			lf.brightness = 0;
     }
   }
 
