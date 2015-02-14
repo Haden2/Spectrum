98b7aee197682f8110a67a14b82ee28d4491fe87
Haden2 <hadext10@gmail.com>
2015-02-14 14:29:22 -0500

    Shifted light position
    I cannot get the flare to turn back on when pressing q.

--- Assets/Scripts/ToggleFlashlight.cs
+++ Assets/Scripts/ToggleFlashlight.cs
@@ -5,17 +5,17 @@ public class ToggleFlashlight : MonoBehaviour
 {
   public float lightIntensity;
 	public  LensFlare lf;
-	public float lensFlare;
+	public float lensBright;
 
   // Update is called once per frame
   void Update ()
   {
     if (Input.GetKeyDown ("q")) 
 	{
-      if (light.intensity == 0) //If the light was off, turn it on.
+      if (light.intensity == 0 && lf.brightness == 0) //If the light was off, turn it on.
        { 
-			light.intensity = lightIntensity;
-			lf.brightness = lensFlare;
+			light.intensity = lightIntensity; //.75
+			lf.brightness = lensBright; //2
 		}
       else
       	 	light.intensity = 0; //If the light was on, turn it off.
