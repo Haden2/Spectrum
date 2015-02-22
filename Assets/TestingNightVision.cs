d23bbb2dcefc2efc10b7a105b25322ec8de93d3a
Haden2 <hadext10@gmail.com>
2015-02-22 03:42:44 -0500

    Large update including addition of night vision
    New script and light to create night vision. Currently I have it set up
    that if you press g you switch from flashlight to night vision and back
    again. Night vision has the grainy look, but non moving. Once I upgrade
    to Unity pro, the static component will move. I may want to darken the
    sides of the night vision because if you think about glasses, they do
    not cover the eyes fully like goggles do. So there would be some space
    in the sides that show up dark. Later I want to make it so you press a
    different button to turn on flashlight and night vision, but one cannot
    be on while the other is on. That is just an if statement. Eventually,
    when the other spectrums are made, I want all of the spectrums to be
    assigned by holding down the middle mouse button and mousing over which
    spectrum you want. Updates to come for night vision still.

---
+++ Assets/TestingNightVision.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+
+public class TestingNightVision : MonoBehaviour
+{
+	public Texture2D texture;
+	public bool isNightVision = false;
+	public float fAlpha = 0.35F;
+	public GameObject NightVisionLight;
+	public GameObject blueLight;
+	public GameObject top;
+	public GameObject secondTop;
+	public GameObject middle;
+	public GameObject secondLowest;
+	public GameObject lowest;
+	public bool isFlashLight;
+
+	void Start()
+	{
+		NightVisionLight.SetActive(false);
+		GetComponent <ToggleFlashlight> ();
+		isFlashLight = true;
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown("g") && (isNightVision == false))
+		{
+			blueLight.SetActive(false);
+			isNightVision = true;
+			isFlashLight = false;
+			NightVisionLight.SetActive(true);
+			//blueLight.SetActive(false);
+		}
+		else if (Input.GetKeyDown("g") && (isNightVision == true))
+		{
+			blueLight.SetActive(true);
+			isNightVision = false;
+			isFlashLight = true;
+			top.SetActive(true);
+			secondTop.SetActive(true);
+			middle.SetActive(true);
+			secondLowest.SetActive(true);
+			lowest.SetActive(true);
+			NightVisionLight.SetActive(false);
+			//blueLight.SetActive(true);
+		}
+	}
+	
+	void OnGUI()
+	{
+		if (isNightVision == true)
+		{
+			blueLight.SetActive(false);
+			top.SetActive(false);
+			secondTop.SetActive(false);
+			middle.SetActive(false);
+			secondLowest.SetActive(false);
+			lowest.SetActive(false);
+
+			var colPreviousGUIColor = GUI.color;
+			GUI.color = new Color(colPreviousGUIColor.r, colPreviousGUIColor.g, colPreviousGUIColor.b, fAlpha);
+			GUI.DrawTexture(new Rect(0.0F, 0.0F, Screen.width, Screen.height), texture);
+		}
+	}
+}
