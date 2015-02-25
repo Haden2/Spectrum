46bd37631357adad450bab7c520138edaadc5609
Haden2 <hadext10@gmail.com>
2015-02-25 04:40:18 -0500

    Inventory
    Not completed. Going through a tutorial that isn't finished yet.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour 
+{
+	public List<Item> inventory = new List<Item>();
+	private ItemDatabase database;
+
+	void Start () 
+	{
+		database = GameObject.FindGameObjectWithTag ("Item Database").GetComponent<ItemDatabase> (); //Grab the ItemDatabase script
+		print (inventory.Count); //How many items are in the inventory?
+		inventory.Add (database.items [0]); //Add the 0 item to the inventory
+		print (inventory.Count); //Now how many are in the inventory?
+		//print(database.items[0].itemName);
+	}
+
+	void OnGUI () 
+	{
+		for(int i = 0; i < inventory.Count; i++)//i is 0, but increase i by one. Keep doing this until i is still < the inventory count
+		{
+			GUI.Label(new Rect(10,i * 30,200,50), inventory[i].itemName); //A square that has slots for the inventory starting at 0 and increasing. i*number makes it so each item is seperated from each other.
+		}
+	}
+}
---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour 
+{
+	public List<Item> items = new List<Item> (); //Items is equal to a new list of items.
+
+	void Start()
+	{
+		items.Add (new Item ("Rock",0,"Used to distract enemies", 1,0, Item.ItemType.Reuse));//Name, item ID, Description, is it resuable, speed?, and what kind of type is it?
+	}
+}
