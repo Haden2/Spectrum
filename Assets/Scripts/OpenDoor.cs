228c28f896a4307a5be93ba77532c25e27a765e6
Haden2 <hadext10@gmail.com>
2015-03-12 03:42:36 -0400

    Moved scripts, fixed error
    Computer crashed and erased some work from yesterday. I put those back
    together and moved the scripts into the scripts folder.

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -4,33 +4,33 @@ using System.Collections;
 public class OpenDoor : MonoBehaviour {
 
 	public GameObject key;
-	bool open;
-	bool closed;
-	bool haveKey;
+	//bool open;
+	//bool closed;
+	//bool haveKey;
 
 	void Start () 
 	{
-		closed = true;
-		open = false;
-		haveKey = false;
+	//	closed = true;
+	//	open = false;
+	//	haveKey = false;
 	}
 
 	void Update () 
 	{
-		if(closed == true && haveKey == true)
+		//if(closed == true && haveKey == true)
 		{
 			//print ("Can Open Door");
 		}
-		if(closed == true && haveKey == false)
+	//	if(closed == true && haveKey == false)
 		{
 			//print ("Need Key First");
 		}
 	}
 	void OpenThisDoor()
 	{
-		if(key.SetActive(true))
+	//	if(key.SetActive(true))
 		{
-			print ("Can Open Door");
+	//		print ("Can Open Door");
 		}
 	}
 }
