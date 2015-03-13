3c53eac6487c30c024c00fe959a72292e3477738
Haden2 <hadext10@gmail.com>
2015-03-13 04:27:19 -0400

    Rotation Hell
    Had to do a lot of research to find a way to get a door to open in the
    negative degrees. It was always turning to 270 when I wanted -90. Then I
    had to do more research in order to find how to change the pivot
    location. It was a nightmare. In the research I found out a way to have
    an object follow the player though. So I threw that script onto "Enemy2"
    for the time being. I had to duplicate the OpenDoor script and change it
    to OpenDoorL in order to get the doors to open differently. This
    probably needs to be optimized, as well as the box colliders. Lots of
    work needs to be done on this still, but it was a full day of research
    just to get it to rotate backwards.

--- Assets/OpenDoorL.cs
+++ Assets/OpenDoorL.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OpenDoor : MonoBehaviour {
-
+public class OpenDoorL : MonoBehaviour {
+	
 	GameObject key;
+	//public BoxCollider button;
 	public Transform door;
 	public Transform pivot;
-	public Vector3 Angle;
+	Vector3 Angle;
+	Vector3 newAngle;
 	bool open;
 	bool closed;
 	bool haveKey;
 	bool rotating;
-
+	
 	void Start () 
 	{
 		closed = true;
@@ -20,7 +22,7 @@ public class OpenDoor : MonoBehaviour {
 		rotating = false; 
 		key = GameObject.FindGameObjectWithTag ("Key");
 	}
-
+	
 	void OnTriggerEnter (Collider other)
 	{
 		if(other.gameObject == key)
@@ -28,44 +30,44 @@ public class OpenDoor : MonoBehaviour {
 			StartCoroutine (WantToOpenDoor());
 		}
 	}
-
+	
 	IEnumerator WantToOpenDoor()
 	{
-		print ("Open Door?");
 		haveKey = true;
 		yield return new WaitForSeconds (0);
 	}
-
+	
 	void Update () 
 	{
 		if(closed == true && haveKey == true && Input.GetKeyDown ("e")/*Event.current.button == 0 && Event.current.type == EventType.mouseUp*/)
 		{
-			print ("Opening Door");
 			rotating = true;
 			open = true;
 		}
-		if(closed == true && haveKey == false && Input.GetKeyDown ("e"))
+		if(closed == true && haveKey == false)
 		{
 			//print ("Need Key First");
 		}
 		if(rotating)
 		{
 			Vector3 Angle = new Vector3(0,270,0);
+			Vector3 newAngle = new Vector3(0,-90,0);
 			if(Vector3.Distance(transform.eulerAngles, Angle) > .01f)
 			{
-				transform.RotateAround(pivot.position, Vector3.Lerp (transform.rotation.eulerAngles, Angle, Time.deltaTime), .5f);
+				transform.RotateAround(pivot.position, newAngle, .5f);
 			}
 			else
 			{
-				transform.eulerAngles = Angle;
+				transform.eulerAngles = newAngle;
 				rotating = false;
 			}
-
+			
 		}
 	}
 	void OpenThisDoor()
 	{
-	
-	}
 
+	}
+	
 }
+
