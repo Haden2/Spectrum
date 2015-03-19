0ccee2e1728f255e918a77966583a381909f1ad9
Haden2 <hadext10@gmail.com>
2015-03-19 16:32:34 -0400

    Condensed scripts/fixed loophole/added button
    Moved AlwaysWatching script. Moved all the different content in
    OpenDoorL into the OpenDoor script. That way I am not duplicating
    scripts. Deleted OpenDoorL script. Added a button to be the trigger
    instead of the door. Added a boolean that makes it so if I am not near
    the collider, pressing E doesn't open the doors again. need to be in the
    collider only to open the door.

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class OpenDoor : MonoBehaviour {
 
 	GameObject key;
-	public Transform door;
-	public Transform pivot;
-	public Vector3 Angle;
-	Vector3 closingAngle;
+	GameObject player;
+	public Transform doorR;
+	public Transform doorL;
+	public Transform pivotR;
+	public Transform pivotL;
+	public Vector3 AngleR;
+	Vector3 closingAngleR;
 	bool open;
 	bool closed;
 	bool closing;
 	bool haveKey;
 	bool rotating;
+	bool nothing;
 
 	void Start () 
 	{
@@ -22,7 +26,9 @@ public class OpenDoor : MonoBehaviour {
 		open = false;
 		haveKey = false;
 		rotating = false; 
+		nothing = true;
 		key = GameObject.FindGameObjectWithTag ("Key");
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 	void OnTriggerEnter (Collider other)
@@ -31,11 +37,24 @@ public class OpenDoor : MonoBehaviour {
 		{
 			StartCoroutine (WantToOpenDoor());
 		}
+		if(other.gameObject == player && haveKey == false)
+		{
+			StartCoroutine (CantOpenDoor());
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == key)
+		{
+			StartCoroutine (DoNothing());
+		}
 	}
 
 	IEnumerator WantToOpenDoor()
 	{
 		print ("Open Door?");
+		nothing = false;
 		haveKey = true;
 		yield return new WaitForSeconds (0);
 	}
@@ -50,49 +69,83 @@ public class OpenDoor : MonoBehaviour {
 		closing = true;
 	}
 
-	//IEnumerator DoorCloses()
-	//{
+	IEnumerator DoNothing()
+	{
+		print ("Nope");
+		nothing = true;
+		yield return new WaitForSeconds (0);
+	}
 
-	//}
+	IEnumerator CantOpenDoor()
+	{
+		haveKey = false;
+		print ("Need Key First");
+		yield return new WaitForSeconds (0);
+	}
 
 	void Update () 
 	{
-		if(closed == true && haveKey == true && Input.GetKeyDown ("e")/*Event.current.button == 0 && Event.current.type == EventType.mouseUp*/)
+		if(closed == true && haveKey == true && nothing == false && Input.GetKeyDown ("e")/*Event.current.button == 0 && Event.current.type == EventType.mouseUp*/)
 		{
 			StartCoroutine(DoorOpens());
 		}
-		if(closed == true && haveKey == false && Input.GetKeyDown ("e"))
-		{
-			//print ("Need Key First");
-		}
+
 		if(rotating)
 		{
-			Vector3 Angle = new Vector3(0,359,0);
-			if(Vector3.Distance(transform.eulerAngles, Angle) > .01f)
+			Vector3 AngleR = new Vector3(0,359,0);
+			Vector3 AngleL = new Vector3(0,0,0);
+			Vector3 newAngleL = new Vector3(0,-90,0);
+		//RIGHT DOOR
+			if(Vector3.Distance(transform.eulerAngles, AngleR) > .01f)
 			{
-				transform.RotateAround(pivot.position, Vector3.Lerp (transform.rotation.eulerAngles, Angle, Time.deltaTime), .5f);
+				transform.RotateAround(pivotR.position, Vector3.Lerp (transform.rotation.eulerAngles, AngleR, Time.deltaTime), .5f);
 			}
 			else
 			{
-				transform.eulerAngles = Angle;
+				transform.eulerAngles = AngleR;
+				rotating = false;
+			}
+		//LEFT DOOR
+			if(Vector3.Distance(doorL.transform.eulerAngles, AngleL) > .01f)
+			{
+				doorL.transform.RotateAround(pivotL.position, newAngleL, .5f);
+			}
+			else
+			{
+				doorL.transform.eulerAngles = AngleL;
 				rotating = false;
 			}
 		}
+		//RIGHT DOOR
 		if(open == false && closing == true)
 			{
-				print ("closing door");
-				Vector3 closingAngle = new Vector3(0,270,0);
-				Vector3 reverseAngle = new Vector3(0,-90,0);
+				Vector3 closingAngleR = new Vector3(0,270,0);
+				Vector3 reverseAngleR = new Vector3(0,-90,0);
 
-				if(Vector3.Distance(transform.eulerAngles, closingAngle) > .01f)
+				if(Vector3.Distance(transform.eulerAngles, closingAngleR) > .01f)
 				{
-				transform.RotateAround(pivot.position, reverseAngle, .5f);
+				transform.RotateAround(pivotR.position, reverseAngleR, .5f);
 				}
 				else
 				{
-					transform.eulerAngles = closingAngle;
+					transform.eulerAngles = closingAngleR;
 					rotating = false;
 				}
 			}
+		//LEFT DOOR
+		if(open == false && closing == true)
+		{
+			Vector3 closingAngleL = new Vector3(0,90,0);
+			
+			if(Vector3.Distance(doorL.transform.eulerAngles, closingAngleL) > .01f)
+			{
+				doorL.transform.RotateAround(pivotL.position, closingAngleL, .5f);
+			}
+			else
+			{
+				doorL.transform.eulerAngles = closingAngleL;
+				rotating = false;
+			}
+		}
 		}
 }
