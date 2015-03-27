90f329fb2836c47827bb5deb835221007129061d
Haden2 <hadext10@gmail.com>
2015-03-27 14:32:56 -0400

    Attempted Animation
    Because the computer overheats every 15 minutes now, I cannot get
    anything done. So its gonna be slow for a little while.

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -7,26 +7,28 @@ public class Elevator : MonoBehaviour {
 	public Transform downPosition;
 	public Transform upPosition;
 	GameObject player;
-	Vector3 up;
-	Vector3 down;
+	Vector3 directionUp;
+	Vector3 directionDown;
 	bool isDown;
 	bool isUp;
 
 
 	void Start()
 	{
-		Vector3 down = downPosition.transform.position;
-		Vector3 up = upPosition.transform.position;
+		Vector3 directionDown = Vector3.down;
+		Vector3 directionUp = Vector3.up;
 		print ("Started");
 		player = GameObject.FindGameObjectWithTag ("Player");
-		StartCoroutine (CanMoveUp ());
+		//StartCoroutine (MoveUp (transform, down, up, 0f));
+		//StartCoroutine (MoveDown (transform, up, down, 0f));
 	}
 	
 	void Update () 
 	{
+		gameObject.transform.Translate (directionUp * Time.smoothDeltaTime);
 		if(isDown)
 		{
-			StartCoroutine(MoveUp (elevator.transform, down, up, 5));
+			//StartCoroutine(MoveUp (elevator.transform, down, up, 5));
 		}
 
 		if(isUp)
@@ -52,8 +54,7 @@ public class Elevator : MonoBehaviour {
 	IEnumerator CanMoveUp()
 	{
 		print ("Got here");
-		isDown = true;
-		isUp = false;
+
 		yield return null;
 		//yield return StartCoroutine(MoveUp (elevator.transform, down, up, 5));
 	}
@@ -66,6 +67,8 @@ public class Elevator : MonoBehaviour {
 
 	IEnumerator MoveUp(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
 	{
+		isDown = false;
+		isUp = true;
 		print ("moving locations");
 		float i = 0.0f;
 		float rate = 1.0f / time;
@@ -77,16 +80,18 @@ public class Elevator : MonoBehaviour {
 		}
 	}
 
-	IEnumerator MoveDown(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+	/*IEnumerator MoveDown(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
 	{
+		isDown = true;
+		isUp = false;
 		float i = 0.0f;
 		float rate = 1.0f / time;
 		while(i < 1.0f) 
 		{
 			i += Time.deltaTime * rate;
 			thisTransform.position = Vector3.Lerp (startPos, endPos, i);
-			yield return null;
+			//yield return StartCoroutine(MoveUp (elevator.transform, down, up, 5));
 		}
-	}
+	}*/
 
 }
