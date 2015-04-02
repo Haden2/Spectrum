334aaa22d5d1c56d66161c828c194ec974b442c4
Haden2 <hadext10@gmail.com>
2015-04-02 03:26:55 -0400

    Elevator works
    Just needed to move the StartCoroutine outside of the while loop. Going
    to start working on the collider now.

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -11,105 +11,96 @@ public class Elevator : MonoBehaviour {
 	Vector3 directionDown;
 	public bool isDown;
 	public bool isUp;
-	public bool moving;
+	public bool isMoving;
 
 
 	void Start()
 	{
 		isDown = true;
 		isUp = false;
-		moving = false;
-	//	Vector3 directionDown = Vector3.down;
-		//Vector3 directionUp = Vector3.up;
-		//player = GameObject.FindGameObjectWithTag ("Player");
+		isMoving = false;
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	void Update () 
 	{
-		//gameObject.transform.Translate (directionUp * Time.smoothDeltaTime);
-		if(isDown)
+		if(isDown == true && isUp == false)
 		{
+		}
+		if(isDown == false && isUp == true)
+		{
+		}
 		
-		}
-
-		if(isUp)
+		if(isMoving)
 		{
-		
+
 		}
 
-		if(moving)
-		{
-			isUp = false;
-			isDown = false;
-		}
-
-		if(isDown == true && isUp == false && moving == false && Input.GetKeyDown ("t"))
+		if(isDown == true && isUp == false && isMoving == false && Input.GetKeyDown ("t"))
 		{
 			StartCoroutine(MoveUp (transform, downPosition, upPosition, 5));
 		}
 
-		if(isDown == false && isUp == true && moving == false && Input.GetKeyDown ("t"))
+		if(isDown == false && isUp == true && isMoving == false && Input.GetKeyDown ("t"))
 		{
 			StartCoroutine(MoveDown (transform, upPosition, downPosition, 5));
 		}
 	}
 
-/*	void OnTriggerEnter (Collider other)
+	void OnTriggerEnter (Collider other)
 	{
 		if(other.gameObject == player)
 		{
-			StartCoroutine (CanMove());
+			StartCoroutine (CanMoveUp());
 		}
-	}
-*/
+}
+
 	IEnumerator CanMoveUp()
 	{
-		if (moving == true) 
-		{
-			yield return new WaitForSeconds (5);
-			isDown = false;
-			isUp = true;
-			moving = false;
-		}
-	}
-	IEnumerator CanMoveDown()
-	{
-		if (moving == true) 
+		if (isMoving == true) 
 		{
 			yield return new WaitForSeconds (5);
 			isDown = true;
 			isUp = false;
-			moving = false;
+			isMoving = false;
+		}
+	}
+	IEnumerator CanMoveDown()
+	{
+		if (isMoving == true) 
+		{
+			yield return new WaitForSeconds (5);
+			isDown = false;
+			isUp = true;
+			isMoving = false;
 		}
 	}
 	
 
 	IEnumerator MoveUp(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
 	{
-		print ("moving up");
-		moving = true;
+		isMoving = true;
+		StartCoroutine(CanMoveDown());
 		float i = 0.0f;
 		float rate = 1.0f / time;
 		while(i < 1.0f) 
 		{
 			i += Time.deltaTime * rate;
 			thisTransform.position = Vector3.Lerp (startPos, endPos, i);
-			StartCoroutine(CanMoveUp());
 			yield return null;
 		}
 }
 
 	IEnumerator MoveDown(Transform thisTransform, Vector3 endPos, Vector3 startPos, float time)
 	{
-		print ("moving down");
-		moving = true;
+		isMoving = true;
+		StartCoroutine(CanMoveUp());
 		float i = 0.0f;
 		float rate = 1.0f / time;
 		while(i < 1.0f) 
 		{
 			i += Time.deltaTime * rate;
 			thisTransform.position = Vector3.Lerp (endPos, startPos, i);
-			StartCoroutine(CanMoveDown());
 			yield return null;
 		}
 	}
