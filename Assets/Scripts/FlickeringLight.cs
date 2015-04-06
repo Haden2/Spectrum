6c41d54be465d47eb7cb914626776bd62e8d13fc
Haden2 <hadext10@gmail.com>
2015-04-06 16:42:35 -0400

    Added cool lights
    Flashing light in hallway. Changed ambiet light to make it dark not
    pitch black. Added exit sign light, will change later. Made flickering
    light script. Looks really cool when you go into the game and turn off
    your flashlight.

---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+	public Light lightBulb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		lightBulb = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		lightBulb.intensity = Random.Range (0f, 2f);
+		lightBulb.range = Random.Range (0f, 30f);
+	}
+}
