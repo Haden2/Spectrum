55ddf87b32f34c12cab46939cdcda3fc0363ee14
Haden2 <hadext10@gmail.com>
2015-05-14 23:59:11 -0400

    Surgeon and Lights
    Added death sequence to the surgeon. Began work on getting the
    flickering lights to go static upon pausing the game. This effect was
    achieved, but I want it to happen with a random number generator and
    varying brightness. Could this be done with the way the flashlight kind
    of worked? Something like setting a variable to be the intensity it is
    at by the time they hit tab?

--- Assets/Scripts/FlickeringLight.cs
+++ Assets/Scripts/FlickeringLight.cs
@@ -3,23 +3,46 @@ using System.Collections;
 
 public class FlickeringLight : MonoBehaviour {
 
+	public Inventory inventory;
 	public Light lightBulb;
 	public Light smallFlicker;
+	public int randomNumber;
 
 	// Use this for initialization
 	void Start () 
 	{
 		lightBulb = GetComponent<Light> ();
+		inventory = GameObject.FindGameObjectWithTag ("Player").GetComponent<Inventory>();
 		smallFlicker = GameObject.Find ("SmallFlicker").GetComponent <Light>();
+		randomNumber = Random.Range (1, 6);
+		print ("Random Number is " + randomNumber);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if(inventory.showInventory == false)
+		{
 		lightBulb.intensity = Random.Range (0f, 2f);
 		lightBulb.range = Random.Range (0f, 30f);
-
+			randomNumber = Random.Range (1,6);
+			print ("Random Number is " + randomNumber);
 		smallFlicker.intensity = Random.Range (3f, 4f);
 		smallFlicker.range = Random.Range (14f, 16f);
+
+		}
+		if(inventory.showInventory == true && randomNumber == 1)
+		{
+			randomNumber = Random.Range (1,6);
+			print("Second Random Number "+ randomNumber);
+			lightBulb.intensity = 1f;
+			lightBulb.range = 15f;
+			smallFlicker.intensity = 3.5f;
+			smallFlicker.range = 15f;
+		}
+	}
+	public void RandomNumber()
+	{
+
 	}
 }
