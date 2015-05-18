fa68d63e27ee2ef863b254a2e8f3719aa8ed5635
Haden2 <hadext10@gmail.com>
2015-05-17 22:10:02 -0400

    Animation and Elevators
    Created animation to use on the doors instead of positioning and
    vectors. This makes life easier. I removed everything from the elevator
    script because it was starting to get complicated by having some
    components be in player controller, and some in elevator. So everything
    is merged into player controller now. Removed a lot of unnecessary and
    dead code. I plan on going back tomorrow to top off all of the elevator
    components. Like make it one button, only have the elevator activate
    when the door is closed, and so on.

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -1,66 +1,61 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 
 public class Elevator : MonoBehaviour {
 
-	public Vector3 elevator;
-	public Vector3 downPosition;
-	public Vector3 upPosition;
-	public Vector3 openPosition;
-	public Vector3 closedPosition;
-	public Vector3 openPosition1;
-	public Vector3 closedPosition1;
-	GameObject player;
-	public GameObject bigDoor;
-	public GameObject littleDoor;
-	float allAboard;
-	public bool isDown;
-	public bool isUp;
-	public bool isMoving;
-	public bool canActivate;
-	public bool closeDoor;
-	public bool stationary;
-	public bool openDoor;
+	//public Vector3 elevator;
+	//public Vector3 downPosition;
+	//public Vector3 upPosition;
+	//public Vector3 openPosition;
+	//public Vector3 closedPosition;
+	//public Vector3 openPosition1;
+	//public Vector3 closedPosition1;
+	//GameObject player;
+	//public GameObject bigDoor;
+	//public GameObject littleDoor;
+	//float allAboard;
+	//public bool isDown;
+	//public bool isUp;
+	//public bool isMoving;
+	//public bool canActivate;
+	//public bool closeDoor;
+	//public bool openDoor;
 
 	void Start()
 	{
-		isDown = true;
-		isUp = false;
-		isMoving = false;
-		closeDoor = true;
-		openDoor = false;
-		canActivate = false;
-		allAboard = 4;
-		elevator = transform.localPosition;
-		downPosition = new Vector3(13, 1, -5);
-		upPosition = new Vector3 (13, 15, -5);
-		openPosition = new Vector3 (10, 2.5f, -1);
-		closedPosition = new Vector3 (10, 2.5f, -5);
-		openPosition1 = new Vector3 (9.85f, 2.5f, -1);
-		closedPosition1 = new Vector3 (9.85f, 2.5f, -3);
+		//isDown = true;
+		//isUp = false;
+		//isMoving = false;
+		//closeDoor = true;
+		//openDoor = false;
+		//canActivate = false;
+		//allAboard = 4;
+		//elevator = transform.localPosition;
+		//downPosition = new Vector3(13, 1, -5);
+		//upPosition = new Vector3 (13, 15, -5);
+		//openPosition = new Vector3 (10, 2.5f, -1);
+		//closedPosition = new Vector3 (10, 2.5f, -5);
+		//openPosition1 = new Vector3 (9.85f, 2.5f, -1);
+		//closedPosition1 = new Vector3 (9.85f, 2.5f, -3);
 
-		player = GameObject.FindGameObjectWithTag ("Player");
+	//	player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	void Update () 
 	{
-		if(closeDoor)
+		//if(closeDoor)
 		{
 			//openDoor = false;
-			StartCoroutine(ElevatorDoor (bigDoor.transform, openPosition, closedPosition, 4));	
-			StartCoroutine(ElevatorDoor (littleDoor.transform, openPosition1, closedPosition1, 4));
+		//	StartCoroutine(ElevatorDoor (bigDoor.transform, openPosition, closedPosition, 4));	
+		//	StartCoroutine(ElevatorDoor (littleDoor.transform, openPosition1, closedPosition1, 4));
 		}
-		if(openDoor)
+	//	if(openDoor)
 		{
 			//closeDoor = false;
-			StartCoroutine(ElevatorDoor (bigDoor.transform, closedPosition, openPosition, 4));	
-			StartCoroutine(ElevatorDoor (littleDoor.transform, closedPosition1, openPosition1, 4));		
+		//	StartCoroutine(ElevatorDoor (bigDoor.transform, closedPosition, openPosition, 4));	
+		//	StartCoroutine(ElevatorDoor (littleDoor.transform, closedPosition1, openPosition1, 4));		
 		}
-		if(stationary)
-		{
-			bigDoor.transform.position = closedPosition;
-			littleDoor.transform.position = closedPosition1;
-		}
+
 		if(isDown == true && isUp == false && isMoving == false && canActivate == true && Input.GetKeyDown ("t"))
 		{
 			StartCoroutine(MoveUp (transform, downPosition, upPosition, 5));
@@ -72,7 +67,7 @@ public class Elevator : MonoBehaviour {
 		}
 	}
 
-	/*void OnTriggerEnter (Collider other)
+	void OnTriggerEnter (Collider other)
 	{
 		if(other == OutsideElevator)
 		if(other.gameObject == player)
@@ -101,7 +96,7 @@ public class Elevator : MonoBehaviour {
 	{
 		canActivate = false;
 		yield return null;
-	}*/
+	}
 
 	IEnumerator CanMoveUp()
 	{
@@ -137,7 +132,7 @@ public class Elevator : MonoBehaviour {
 			thisTransform.position = Vector3.Lerp (startPos, endPos, i);
 			yield return null;
 		}
-}
+	}
 
 	IEnumerator MoveDown(Transform thisTransform, Vector3 endPos, Vector3 startPos, float time)
 	{
@@ -169,7 +164,7 @@ public class Elevator : MonoBehaviour {
 		print("This is when it activates");
 	}
 
-	/*IEnumerator ElevatorDoorClose(Transform thatTransform, Vector3 endArea, Vector3 startArea, float timing)
+	IEnumerator ElevatorDoorClose(Transform thatTransform, Vector3 endArea, Vector3 startArea, float timing)
 	{
 		float h = 0.0f;
 		float rot = 1.0f / timing;
@@ -181,5 +176,5 @@ public class Elevator : MonoBehaviour {
 			yield return null;
 			//stationary = true;
 		}
-	}*/
-}
+	}
+}*/
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public Elevator elevator;
+	GameObject elevator;
 	public Inventory inventory;
+	GameObject bigDoor;
+	GameObject littleDoor;
 	public bool elevatorDoor;
 	public bool inElevator;
+	public bool doorIsClosed;
+	public bool canActive;
+	public bool isDown;
+	public bool isUp;
+	public bool isMoving;
+	public Vector3 downPosition;
+	public Vector3 upPosition;
 
 	void Start()
 	{
-		elevator = GameObject.FindGameObjectWithTag ("Elevator").GetComponent<Elevator> ();
+		elevator = GameObject.FindGameObjectWithTag ("Elevator");
 		inventory = GetComponent<Inventory> ();
+		bigDoor = GameObject.FindGameObjectWithTag ("BigDoor");
+		littleDoor = GameObject.FindGameObjectWithTag ("LittleDoor");
+		downPosition = new Vector3(13, 1, -5);
+		upPosition = new Vector3 (13, 15, -5);
+		isDown = true;
+		isUp = false;
+		isMoving = false;
 	}
 
 	void OnTriggerEnter (Collider other)
@@ -24,7 +40,7 @@ public class PlayerController : MonoBehaviour {
 		if(elevatorDoor && inventory.activeElevatorKey)
 		{
 			print ("Engage the Doors motherfucker");
-			elevator.canActivate = true;
+			canActive = true;
 		}
 	}
 	void OnTriggerExit(Collider other)
@@ -37,10 +53,66 @@ public class PlayerController : MonoBehaviour {
 
 	void Update()
 	{
-		if(elevatorDoor && inventory.activeElevatorKey && elevator.canActivate == true && Input.GetKeyDown("e"))
+		if(elevatorDoor && inventory.activeElevatorKey && canActive == true && Input.GetKeyDown("e"))
 		{
-			elevator.closeDoor = false;
-			elevator.openDoor = true;
+			littleDoor.GetComponent<Animation>().Play("SmallElevatorDoor");
+			bigDoor.GetComponent<Animation>().Play("ElevatorDoor");
+		}
+		if(isDown == true && isUp == false && isMoving == false && canActive == true && Input.GetKeyDown ("t"))
+		{
+			StartCoroutine(MoveUp (elevator.transform, downPosition, upPosition, 5));
+		}
+		if(isDown == false && isUp == true && isMoving == false && canActive == true && Input.GetKeyDown ("t"))
+		{
+			StartCoroutine(MoveDown (elevator.transform, upPosition, downPosition, 5));
+		}
+	}
+	IEnumerator CanMoveUp()
+	{
+		if (isMoving == true) 
+		{
+			yield return new WaitForSeconds (5);
+			isDown = true;
+			isUp = false;
+			isMoving = false;
+		}
+	}
+	IEnumerator CanMoveDown()
+	{
+		if (isMoving == true) 
+		{
+			yield return new WaitForSeconds (5);
+			isDown = false;
+			isUp = true;
+			isMoving = false;
+		}
+	}
+
+	IEnumerator MoveUp(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+	{
+		isMoving = true;
+		StartCoroutine(CanMoveDown());
+		float i = 0.0f;
+		float rate = 1.0f / time;
+		while(i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp (startPos, endPos, i);
+			yield return null;
+		}
+	}
+
+	IEnumerator MoveDown(Transform thisTransform, Vector3 endPos, Vector3 startPos, float time)
+	{
+		isMoving = true;
+		StartCoroutine(CanMoveUp());
+		float i = 0.0f;
+		float rate = 1.0f / time;
+		while(i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp (endPos, startPos, i);
+			yield return null;
 		}
 	}
 }
