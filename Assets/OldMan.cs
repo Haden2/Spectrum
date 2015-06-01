68f1e663472847de10789c86096ab424d8735b74
Haden2 <hadext10@gmail.com>
2015-06-01 12:42:24 -0400

    Bad Update
    Full day spent to the old man. Nothing really worked though. Saving this
    anyway because I need to restart my computer and I didn't want any funny
    business.

---
+++ Assets/OldMan.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldMan : MonoBehaviour {
+
+	PlayerController player;
+	NavMeshAgent nav;
+	public Transform hallway;
+	public Transform playerDestination;
+	public Transform oldmanLocation;
+	public Transform keepMoving;
+	public Vector3 previousLocation;
+	float speed;
+	float walkRadius;
+	bool atPlayer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		walkRadius = 10;
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+		playerDestination = GameObject.FindGameObjectWithTag ("Player").transform;
+		hallway = GameObject.FindGameObjectWithTag ("Hallway").transform;
+		nav = GetComponent<NavMeshAgent>();
+		speed = 10;
+		keepMoving.position = playerDestination.transform.position + transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+		//Vector3 worldDeltaPosition = playerDestination.position - transform.position;
+		//randomDirection += transform.position;
+		//NavMeshHit hit;
+		//NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
+		//Vector3 finalPosition = hit.position;
+		//print (worldDeltaPosition);
+		//if(player.oldManSeen)
+		{
+			//StartCoroutine (Charge());
+			//previousLocation = playerDestination.transform.position;
+			//nav.speed = 2;
+			//nav.SetDestination(playerDestination.position);
+		//	if(nav.remainingDistance <= .5f)
+			{
+				//transform.rotation = Quaternion.LookRotation(transform.position - playerDestination.position);
+				//Vector3 runTo = transform.position + transform.forward * speed;
+				//NavMeshHit hit;
+				//NavMesh.SamplePosition(runTo, out hit, 5, 1 << NavMesh.GetAreaFromName("Walkable"));
+				//nav.SetDestination(hit.position);
+			//	atPlayer = true;
+			//	print ("here");
+			}
+		}
+
+		//if(atPlayer /*&& player.oldManSeen == false*/)
+		{
+			//nav.SetDestination(hallway.position);
+		}
+	}
+
+	//IEnumerator Charge()
+	//{
+		//nav.SetDestination(playerDestination.position);
+		//yield return null; 
+	//}
+}
---
+++ Assets/Wander.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Creates wandering behaviour for a CharacterController.
+/// </summary>
+public class Wander : MonoBehaviour
+{
+	public float speed = 1;
+	public float directionChangeInterval = 1;
+	public float maxHeadingChange = 10;
+	
+	float heading;
+	Vector3 targetRotation;
+	
+	void Awake ()
+	{
+		// Set random initial rotation
+		heading = Random.Range(0, 360);
+		transform.eulerAngles = new Vector3(0, heading, 0);
+		
+		StartCoroutine(NewHeading());
+	}
+	
+	void Update ()
+	{
+		transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+		var forward = transform.TransformDirection(Vector3.forward);
+		transform.TransformDirection (Vector3.forward * speed);	
+	}
+	
+	/// <summary>
+	/// Repeatedly calculates a new direction to move towards.
+	/// Use this instead of MonoBehaviour.InvokeRepeating so that the interval can be changed at runtime.
+	/// </summary>
+	IEnumerator NewHeading ()
+	{
+		while (true) {
+			NewHeadingRoutine();
+			yield return new WaitForSeconds(directionChangeInterval);
+		}
+	}
+	
+	/// <summary>
+	/// Calculates a new direction to move towards.
+	/// </summary>
+	void NewHeadingRoutine ()
+	{
+
+		targetRotation = new Vector3(0, heading, 0);
+	}
+}
