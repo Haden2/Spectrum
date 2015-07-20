cf72999500e40772c7d6305916a76800f2e76914
Haden2 <hadext10@gmail.com>
2015-07-19 23:07:05 -0400

    Added BlueRay
    Made rays only cast when in the light. Added the blue line that follows
    the player. Other additional changes.

--- Assets/Scripts/LightsOut.cs
+++ Assets/Scripts/LightsOut.cs
@@ -23,10 +23,15 @@ public class LightsOut : MonoBehaviour
 	public float targetAngle;
 	public float distance;
 	public float distanceFromLight;
-	public float alpha;
 	public float hypotenuseAngle;
+	public float globalBlueAngle;
+	public float globalOrangeAngle;
+	public float globalLeftOrangeAngle;
+	public float alphaChangeRate;
 
-	public Vector3 rightAngle;
+	//public Vector3 rightAngle;
+	public Vector3 endDirection;
+	public Vector3 newDirection;
 
 	void Awake()
 	{
@@ -40,57 +45,72 @@ public class LightsOut : MonoBehaviour
 	{
 		if(target != null) //If the target variable isn't blank. Putting things outside of this if statement creates issues because it takes a couple seconds for the game to find all of the lights.
 		{
+			alphaChangeRate = (distance/(distance + 5));
 			targetLight = target.gameObject.GetComponent<Light> (); //grabbed the gameobjects light component.
+			globalBlueAngle = targetLight.transform.eulerAngles.y;
+			globalOrangeAngle = globalBlueAngle + hypotenuseAngle;
+			globalLeftOrangeAngle = globalBlueAngle - hypotenuseAngle;
+			endDirection = gameObject.transform.position - target.transform.position;
+			//float step = speed * Time.deltaTime;
+			newDirection = Vector3.RotateTowards (target.transform.forward, endDirection, 1,10);
+			//print (globalOrangeAngle);
+			//print (globalLeftOrangeAngle);
 			if(targetLight.name == ("LMspotlight")) //if that closest light is called spotlight
 			{
-				targetLight.transform.Rotate(Vector3.up*Time.deltaTime*50); //spin the light around. *50 is the speed. Increase to make it go faster
+				//targetLight.transform.Rotate(Vector3.up*Time.deltaTime*50); //spin the light around. *50 is the speed. Increase to make it go faster
 			}
 			if(targetLight.type == LightType.Spot) //if it is a spotlight in general
 			{
-				Quaternion rotation = Quaternion.AngleAxis(targetAngle * .5f, targetLight.transform.up); //this is the right side of the circle.
-				Quaternion rotationL = Quaternion.AngleAxis(-targetAngle * .5f, targetLight.transform.up); //this is the left edge of the circle.
-				RaycastHit hit; //dead center spot
-				RaycastHit rightHit; //the raycast on the right edge
-				RaycastHit leftHit; // the raycast on the left edge
 				Vector3 rayDirection = targetLight.transform.position - transform.position; //the distance from the light to the light monster. 
 				Ray ray = new Ray(targetLight.transform.position, targetLight.transform.forward); //ray from light to player
-				Ray rightRay = new Ray(targetLight.transform.position, rotation * targetLight.transform.forward); //ray from light to right side of circle
-				Ray leftRay = new Ray(targetLight.transform.position, rotationL * targetLight.transform.forward); //ray from light to left side of circle
-		
-				if(Physics.Raycast(ray, out hit, targetRange)) //if the raycast hit something within range. targetRange matches range of spotlight.
+				RaycastHit hit; //dead center spot
+				Debug.DrawRay(targetLight.transform.position, targetLight.transform.forward * targetRange, Color.yellow); //Draw this beam out.
+
+				if(inLight)
 				{
-				//	print (rayDirection); //the distance from light to light monster in this case is 9. This is the Adjacent leg of the triangle. I will get more into that in github
-					//print (hit.transform);   says what I hit
-					if(hit.transform.name == "LightMonster") //did the middle ray hit the monster?
+					Quaternion rotation = Quaternion.AngleAxis(targetAngle * .5f, targetLight.transform.up); //this is the right side of the circle.
+					Quaternion rotationL = Quaternion.AngleAxis(-targetAngle * .5f, targetLight.transform.up); //this is the left edge of the circle.
+					RaycastHit rightHit; //the raycast on the right edge
+					RaycastHit leftHit; // the raycast on the left edge
+					RaycastHit blueHit;
+					Ray rightRay = new Ray(targetLight.transform.position, rotation * targetLight.transform.forward); //ray from light to right side of circle
+					Ray leftRay = new Ray(targetLight.transform.position, rotationL * targetLight.transform.forward); //ray from light to left side of circle
+					Ray blueRay = new Ray(target.transform.position, newDirection);
+					Debug.DrawRay(targetLight.transform.position, rotation * targetLight.transform.forward * targetRange, Color.yellow); //show right ray
+					Debug.DrawRay(targetLight.transform.position, rotationL * targetLight.transform.forward * targetRange, Color.yellow); //show left ray
+					Debug.DrawRay(target.transform.position, newDirection, Color.blue);
+				
+					if(Physics.Raycast(ray, out hit, targetRange)) //if the raycast hit something within range. targetRange matches range of spotlight.
 					{
-						lightMonsterSeen = true; //yeah, monster is seen.
-					}
-					if(hit.transform.name != "LightMonster") //is it not hitting the monster?
-					{
-						lightMonsterSeen = false; //monster is not being directly looked at.
-					}
-					Debug.DrawRay(targetLight.transform.position, targetLight.transform.forward * targetRange); //Draw this beam out.
-				}
-				if(Physics.Raycast(rightRay, out rightHit, targetRange)) // if the right side of the light hits something
-				{
-					if(rightHit.transform.name == "LightMonster") //if it hits the monster
-					{
-						distanceFromLight = rightHit.distance;
-						Debug.DrawRay(targetLight.transform.position, rotation * targetLight.transform.forward * targetRange, Color.blue); //show right ray
-						Debug.DrawRay(targetLight.transform.position, rotationL * targetLight.transform.forward * targetRange, Color.blue); //show left ray
-						if(distanceFromLight >5)
+					//	print (rayDirection); //the distance from light to light monster in this case is 9. This is the Adjacent leg of the triangle. I will get more into that in github
+						//print (hit.transform);   says what I hit
+						if(hit.transform.name == "LightMonster") //did the middle ray hit the monster?
 						{
-							//print (newColor.a);
-							//newColor.a = alpha;
-							this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1,.5f);
+							lightMonsterSeen = true; //yeah, monster is seen.
 						}
-						if(distanceFromLight < 5)
+						if(hit.transform.name != "LightMonster") //is it not hitting the monster?
 						{
-							this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1,.2f);
+							lightMonsterSeen = false; //monster is not being directly looked at.
 						}
-					//	print (rightHit.transform.position); // the global position of the light monster
-						//print (hit.transform.position); // the global position of the wall behind the light monster
-						//rightAngle = rightHit.transform.position - hit.transform.position;   All this did was say that the wall is at this position, and the enemy is at this position. Doesn't give a good value.
+					}
+					if(Physics.Raycast(blueRay, out blueHit, targetRange)) // if the right side of the light hits something
+					{
+						if(blueHit.transform.name == "LightMonster") //if it hits the monster
+						{
+							distanceFromLight = blueHit.distance;
+
+							if(distanceFromLight >5)
+							{
+								this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1,.5f);
+							}
+							if(distanceFromLight < 5)
+							{
+								this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1,.2f);
+							}
+							//	print (rightHit.transform.position); // the global position of the light monster
+							//print (hit.transform.position); // the global position of the wall behind the light monster
+							//rightAngle = rightHit.transform.position - hit.transform.position;   All this did was say that the wall is at this position, and the enemy is at this position. Doesn't give a good value.
+						}
 					}
 				}
 				if ((Vector3.Angle(rayDirection, -targetLight.transform.forward)) <= targetAngle * 0.5f) // if the enemy is in the spotlight at all
@@ -112,6 +132,7 @@ public class LightsOut : MonoBehaviour
 			targetRange = targetLight.range;  //the targetRange is how far the light goes. The lights range.
 			targetAngle = targetLight.spotAngle; // the targetAngle is how wide the circle is
 			hypotenuseAngle = targetAngle/2;
+
 		}
 	} 
 
