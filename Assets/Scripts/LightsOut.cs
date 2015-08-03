12aeb501bfc413ce4e284e5de70b31f57da80694
Haden2 <hadext10@gmail.com>
2015-08-03 02:08:38 -0400

    Green Angle, combining lights
    I got the angle from the blue line to the mid and the sides. The issue
    now is that there isn't much of a smooth transition with the light. Need
    to work out the math between green angle and alpha change rate. The two
    need to balance.

--- Assets/Scripts/LightsOut.cs
+++ Assets/Scripts/LightsOut.cs
@@ -15,9 +15,12 @@ public class LightsOut : MonoBehaviour
 	public GameObject target;
 	public GameObject[] lights;
 	public GameObject closest;
+	public GameObject lightLocation;
 
 	public bool lightMonsterSeen;
 	public bool inLight;
+	public bool onRight;
+	public bool onLeft;
 	
 	public float targetRange;
 	public float targetAngle;
@@ -30,6 +33,7 @@ public class LightsOut : MonoBehaviour
 	public float globalBlueLineAngle;
 	public float alphaChangeRate;
 	public float intensityAlpha;
+	public float greenAngle;
 
 	//public Vector3 rightAngle;
 	public Vector3 endDirection;
@@ -40,6 +44,7 @@ public class LightsOut : MonoBehaviour
 		//rendererer = gameObject.GetComponent<MeshRenderer> ();
 		fadeMaterial = gameObject.GetComponent<Renderer>().material;
 		newColor = fadeMaterial.color;
+		lightLocation = GameObject.Find("LightLocation");
 		InvokeRepeating("FindClosestLight", 0.5f,0.5f); //Calls the function to find the nearest light.
 	}
 
@@ -53,13 +58,13 @@ public class LightsOut : MonoBehaviour
 			globalRightAngle = globalMidAngle + blueAngle;
 			globalLeftOrangeAngle = globalMidAngle - blueAngle;
 			endDirection = gameObject.transform.position - target.transform.position;
-			//float step = speed * Time.deltaTime;
 			newDirection = Vector3.RotateTowards (target.transform.forward, endDirection, 1,10);
+			lightLocation.transform.position = target.transform.position;
 			//print (globalOrangeAngle);
 			//print (globalLeftOrangeAngle);
 			if(targetLight.name == ("LMspotlight")) //if that closest light is called spotlight
 			{
-				//targetLight.transform.Rotate(Vector3.up*Time.deltaTime*50); //spin the light around. *50 is the speed. Increase to make it go faster
+				targetLight.transform.Rotate(Vector3.up*Time.deltaTime*50); //spin the light around. *50 is the speed. Increase to make it go faster
 			}
 			if(targetLight.type == LightType.Spot) //if it is a spotlight in general
 			{
@@ -67,7 +72,7 @@ public class LightsOut : MonoBehaviour
 				Ray ray = new Ray(targetLight.transform.position, targetLight.transform.forward); //ray from light to player
 				RaycastHit hit; //dead center spot
 				Debug.DrawRay(targetLight.transform.position, targetLight.transform.forward * targetRange, Color.yellow); //Draw this beam out.
-				this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1, alphaChangeRate);
+				//this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1, alphaChangeRate /*- greenAngle*/);
 
 				if(inLight)
 				{
@@ -79,10 +84,13 @@ public class LightsOut : MonoBehaviour
 					RaycastHit blueHit;
 					Ray rightRay = new Ray(targetLight.transform.position, rotation * targetLight.transform.forward); //ray from light to right side of circle
 					Ray leftRay = new Ray(targetLight.transform.position, rotationL * targetLight.transform.forward); //ray from light to left side of circle
-					Ray blueRay = new Ray(target.transform.position, newDirection);
+					Ray blueRay = new Ray(lightLocation.transform.position, newDirection);
 					Debug.DrawRay(targetLight.transform.position, rotation * targetLight.transform.forward * targetRange, Color.yellow); //show right ray
 					Debug.DrawRay(targetLight.transform.position, rotationL * targetLight.transform.forward * targetRange, Color.yellow); //show left ray
 					Debug.DrawRay(target.transform.position, newDirection, Color.blue);
+					lightLocation.transform.rotation = Quaternion.LookRotation(newDirection);
+					print(targetLight.intensity);
+					print(targetLight.name);
 
 				
 					if(Physics.Raycast(ray, out hit, targetRange)) //if the raycast hit something within range. targetRange matches range of spotlight.
@@ -103,19 +111,41 @@ public class LightsOut : MonoBehaviour
 						if(blueHit.transform.name == "LightMonster") //if it hits the monster
 						{
 							distanceFromLight = blueHit.distance;
-							globalBlueLineAngle = blueRay.direction.normalized.y;
-							Quaternion globalBlueAngle = Quaternion.identity;
-							globalBlueAngle.eulerAngles = (transform.eulerAngles);
-							print(globalBlueAngle.eulerAngles.x);
+							globalBlueLineAngle = lightLocation.transform.eulerAngles.y;
+							if(globalBlueLineAngle >= globalMidAngle && globalBlueLineAngle <= globalRightAngle)
+							{
+								onRight = true;
+								onLeft = false;
+							}
+							if(globalBlueLineAngle <= globalMidAngle && globalBlueLineAngle >= globalLeftOrangeAngle)
+							{
+								onRight = false;
+								onLeft = true;
+							}
+							if(onRight)
+							{
+								//greenAngle = (globalRightAngle - globalBlueLineAngle)/blueAngle;
+								greenAngle = (globalBlueLineAngle - globalMidAngle)/blueAngle;
+								this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1, ((greenAngle * alphaChangeRate)));
+							}
+							if(onLeft)
+							{
+								greenAngle = (globalMidAngle - globalBlueLineAngle)/blueAngle;
+								this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1, ((greenAngle * alphaChangeRate)));
+							}
 							//print (rightHit.transform.position); // the global position of the light monster
 							//print (hit.transform.position); // the global position of the wall behind the light monster
 							//rightAngle = rightHit.transform.position - hit.transform.position;   All this did was say that the wall is at this position, and the enemy is at this position. Doesn't give a good value.
 						}
 					}
 				}
+				if(alphaChangeRate == 1)
+				{
+					inLight = false;
+				}
 				if(inLight == false)
 				{
-					alphaChangeRate = 1;
+					this.GetComponent<MeshRenderer>().material.color = new Color (1,1,1,1);
 				}
 				if ((Vector3.Angle(rayDirection, -targetLight.transform.forward)) <= targetAngle * 0.5f) // if the enemy is in the spotlight at all
 				{
@@ -125,18 +155,9 @@ public class LightsOut : MonoBehaviour
 					inLight =false; //otherwise, dont show it
 				}
 			}
-			if(targetLight.intensity >=2) //if that light is bright
-			{
-
-			}
-			if(targetLight.intensity <2) // if that light is dim
-			{
-
-			}
 			targetRange = targetLight.range;  //the targetRange is how far the light goes. The lights range.
 			targetAngle = targetLight.spotAngle; // the targetAngle is how wide the circle is
 			blueAngle = targetAngle/2;
-
 		}
 	} 
 
